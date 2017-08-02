b7a2ea53ae39893c8d370d190c31c0ad766e4852
timuranosov <tumyp.anosov@gmail.com>
2017-08-02 11:19:32 +0300

    Refactoring

--- vendor-code-finder/src/test/java/qcha/arfind/excel/qcha/arfind/ExcelTextFinderOnXlsTest.java
+++ vendor-code-finder/src/test/java/qcha/arfind/excel/qcha/arfind/ExcelTextFinderOnXlsTest.java
@@ -9,7 +9,6 @@ import qchar.arfind.excel.ExcelTextFinder;
 public class ExcelTextFinderOnXlsTest {
 
     private ExcelTextFinder parser;
-    private String matchString;
 
     @Before
     public void setUp() throws Exception {
@@ -24,137 +23,110 @@ public class ExcelTextFinderOnXlsTest {
 
     @Test
     public void caseSensitivityTest() throws Exception {
-        matchString = "ВАГОНКА";
-        Assert.assertEquals(20, parser.findMatches(matchString).size());
-
-        matchString = "вагонка";
-        Assert.assertEquals(20, parser.findMatches(matchString).size());
-
-        matchString = "Вагонка";
-        Assert.assertEquals(20, parser.findMatches(matchString).size());
+        //upper case
+        Assert.assertEquals(20, parser.findMatches("ВАГОНКА").size());
+        //lower case
+        Assert.assertEquals(20, parser.findMatches("вагонка").size());
+        //default
+        Assert.assertEquals(20, parser.findMatches("Вагонка").size());
 
     }
 
     @Test
     public void oneNotFullWordTest() throws Exception {
-        matchString = "Бр";
-        Assert.assertEquals(60, parser.findMatches(matchString).size());
+        Assert.assertEquals(60, parser.findMatches("Бр").size());
     }
 
     @Test
     public void anotherTest() {
-        matchString = "Вагонка сорта А";
-        Assert.assertEquals(7, parser.findMatches(matchString).size());
+        //part of phrase
+        Assert.assertEquals(7, parser.findMatches("Вагонка сорта А").size());
     }
 
     @Test
     public void stringWithoutSpacesTest() throws Exception {
-        matchString = "RAL3005";
-        Assert.assertEquals(22, parser.findMatches(matchString).size());
+        Assert.assertEquals(22, parser.findMatches("RAL3005").size());
     }
 
     @Test
     public void firstStringWithOneSpaceTest() throws Exception {
-        matchString = "Соединитель желоба";
-        Assert.assertEquals(4, parser.findMatches(matchString).size());
+        Assert.assertEquals(4, parser.findMatches("Соединитель желоба").size());
     }
 
     @Test
     public void firstStringWithTrimmedLastWord() throws Exception {
-        matchString = "Соединитель жел";
-        Assert.assertEquals(5, parser.findMatches(matchString).size());
+        Assert.assertEquals(5, parser.findMatches("Соединитель жел").size());
     }
 
     @Test
     public void secondStringWithTrimmedLastWord() throws Exception {
-        matchString = "Соединитель ж";
-        Assert.assertEquals(5, parser.findMatches(matchString).size());
+        Assert.assertEquals(5, parser.findMatches("Соединитель ж").size());
     }
 
     @Test
     public void secondStringWithOneTest() throws Exception {
-        matchString = "Труба водосточная";
-        Assert.assertEquals(14, parser.findMatches(matchString).size());
+        Assert.assertEquals(14, parser.findMatches("Труба водосточная").size());
     }
 
     @Test
     public void thirdStringWithTrimmedLastWord() throws Exception {
-        matchString = "Труба водо";
-        Assert.assertEquals(14, parser.findMatches(matchString).size());
+        Assert.assertEquals(14, parser.findMatches("Труба водо").size());
     }
 
     @Test
     public void fourthStringWithTrimmedLastWord() throws Exception {
-        matchString = "Труба в";
-        Assert.assertEquals(14, parser.findMatches(matchString).size());
+        Assert.assertEquals(14, parser.findMatches("Труба в").size());
     }
 
     @Test
     public void fifthStringWithTrimmedLastWord() throws Exception {
-        matchString = "Миксер для красок";
-        Assert.assertEquals(2, parser.findMatches(matchString).size());
+        Assert.assertEquals(2, parser.findMatches("Миксер для красок").size());
     }
 
     @Test
     public void sixthStringWithTrimmedLastWord() throws Exception {
-        matchString = "Миксер для кр";
-        Assert.assertEquals(2, parser.findMatches(matchString).size());
+        Assert.assertEquals(2, parser.findMatches("Миксер для кр").size());
     }
 
     @Test
     public void seventhStringWithTrimmedLastWord() throws Exception {
-        matchString = "Миксер для";
-        Assert.assertEquals(4, parser.findMatches(matchString).size());
+        Assert.assertEquals(4, parser.findMatches("Миксер для").size());
     }
 
     @Test
     public void stringWithSeveralSpacesTest() throws Exception {
-        matchString = "Держатель желоба на стену";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Держатель желоба на стену").size());
     }
 
     @Test
     public void fullNameTest() throws Exception {
-        matchString = "Прокладка из ДСП 3,5х1,75";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Прокладка из ДСП 3,5х1,75").size());
     }
 
     @Test
     public void stringWithDifficultLetters() throws Exception {
-        matchString = "Жёлоб";
-        Assert.assertEquals(45, parser.findMatches(matchString).size());
+        Assert.assertEquals(45, parser.findMatches("Жёлоб").size());
 
-        matchString = "Желоб";
-        Assert.assertEquals(45, parser.findMatches(matchString).size());
+        Assert.assertEquals(45, parser.findMatches("Желоб").size());
 
-        matchString = "Крепеж";
-        Assert.assertEquals(39, parser.findMatches(matchString).size());
+        Assert.assertEquals(39, parser.findMatches("Крепеж").size());
 
-        matchString = "Крепёж";
-        Assert.assertEquals(39, parser.findMatches(matchString).size());
+        Assert.assertEquals(39, parser.findMatches("Крепёж").size());
 
-        matchString = "Черная";
-        Assert.assertEquals(14, parser.findMatches(matchString).size());
+        Assert.assertEquals(14, parser.findMatches("Черная").size());
 
-        matchString = "Чёрная";
-        Assert.assertEquals(14, parser.findMatches(matchString).size());
+        Assert.assertEquals(14, parser.findMatches("Чёрная").size());
 
-        matchString = "Темный";
-        Assert.assertEquals(4, parser.findMatches(matchString).size());
+        Assert.assertEquals(4, parser.findMatches("Темный").size());
 
-        matchString = "Тёмный";
-        Assert.assertEquals(4, parser.findMatches(matchString).size());
+        Assert.assertEquals(4, parser.findMatches("Тёмный").size());
 
-        matchString = "Плёнка";
-        Assert.assertEquals(11, parser.findMatches(matchString).size());
+        Assert.assertEquals(11, parser.findMatches("Плёнка").size());
 
-        matchString = "Пленка";
-        Assert.assertEquals(11, parser.findMatches(matchString).size());
+        Assert.assertEquals(11, parser.findMatches("Пленка").size());
 
-        matchString = "трёхраспорный";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("трёхраспорный").size());
 
-        matchString = "трехраспорный";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("трехраспорный").size());
     }
 }
--- vendor-code-finder/src/test/java/qcha/arfind/excel/qcha/arfind/ExcelTextFinderOnXlsxTest.java
+++ vendor-code-finder/src/test/java/qcha/arfind/excel/qcha/arfind/ExcelTextFinderOnXlsxTest.java
@@ -9,7 +9,6 @@ import qchar.arfind.excel.ExcelTextFinder;
 public class ExcelTextFinderOnXlsxTest {
 
     private ExcelTextFinder parser;
-    private String matchString;
 
     @Before
     public void setUp() throws Exception {
@@ -24,92 +23,76 @@ public class ExcelTextFinderOnXlsxTest {
 
     @Test
     public void caseSensitivityTest() throws Exception {
-        String matchString = "КЛАПАН";
-        Assert.assertEquals(30, parser.findMatches(matchString).size());
-
-        matchString = "клапан";
-        Assert.assertEquals(30, parser.findMatches(matchString).size());
-
-        matchString = "Клапан";
-        Assert.assertEquals(30, parser.findMatches(matchString).size());
+        //upper case
+        Assert.assertEquals(30, parser.findMatches("КЛАПАН").size());
+        //lower case
+        Assert.assertEquals(30, parser.findMatches("клапан").size());
+        //default
+        Assert.assertEquals(30, parser.findMatches("Клапан").size());
 
     }
 
     @Test
     public void oneNotFullWordTest() throws Exception {
-        matchString = "кл";
-        Assert.assertEquals(128, parser.findMatches(matchString).size());
+        Assert.assertEquals(128, parser.findMatches("кл").size());
     }
 
     @Test
     public void testWithStringInQuotes() {
-        matchString = "Клапан нижний \"Ани\"";
-        Assert.assertEquals(2, parser.findMatches(matchString).size());
+        Assert.assertEquals(2, parser.findMatches("Клапан нижний \"Ани\"").size());
     }
 
     @Test
     public void stringWithoutSpacesTest() throws Exception {
-        matchString = "НСС-1101";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("НСС-1101").size());
     }
 
     @Test
     public void firstStringWithOneSpaceTest() throws Exception {
-        matchString = "Груша сантехническая";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Груша сантехническая").size());
     }
 
     @Test
     public void firstStringWithTrimmedLastWord() throws Exception {
-        matchString = "Груша сантех";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Груша сантех").size());
     }
 
     @Test
     public void secondStringWithTrimmedLastWord() throws Exception {
-        matchString = "Груша сан";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Груша сан").size());
     }
 
     @Test
     public void secondStringWithOneTest() throws Exception {
-        matchString = "Колонка боковая латунь";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Колонка боковая латунь").size());
     }
 
     @Test
     public void thirdStringWithTrimmedLastWord() throws Exception {
-        matchString = "Колонка боковая";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Колонка боковая").size());
     }
 
     @Test
     public void fourthStringWithTrimmedLastWord() throws Exception {
-        matchString = "Колонка бок";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Колонка бок").size());
     }
 
     @Test
     public void stringWithDifficultLetters() throws Exception {
-        matchString = "Ёрш";
-        Assert.assertEquals(16, parser.findMatches(matchString).size());
+        Assert.assertEquals(16, parser.findMatches("Ёрш").size());
 
-        matchString = "Ерш";
-        Assert.assertEquals(16, parser.findMatches(matchString).size());
+        Assert.assertEquals(16, parser.findMatches("Ерш").size());
 
-        matchString = "Тётётёр";
-        Assert.assertEquals(6, parser.findMatches(matchString).size());
+        Assert.assertEquals(6, parser.findMatches("Тётётёр").size());
 
-        matchString = "Тететер";
-        Assert.assertEquals(6, parser.findMatches(matchString).size());
+        Assert.assertEquals(6, parser.findMatches("Тететер").size());
 
     }
 
 
     @Test
     public void stringWithSeveralSpacesTest() throws Exception {
-        matchString = "Ремкомплект ПСМ Имп 1/2 металл КРЕСТ";
-        Assert.assertEquals(1, parser.findMatches(matchString).size());
+        Assert.assertEquals(1, parser.findMatches("Ремкомплект ПСМ Имп 1/2 металл КРЕСТ").size());
     }
 
 }
