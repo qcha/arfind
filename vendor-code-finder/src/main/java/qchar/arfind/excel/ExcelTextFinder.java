07cd65ac2cfed49a9a965d34c4ac9ed3155b84ea
Aleksandr Kuchuk <aarexer@gmail.com>
2017-08-03 18:22:33 +0300

    Sheet finding

--- vendor-code-finder/src/main/java/qchar/arfind/excel/ExcelTextFinder.java
+++ vendor-code-finder/src/main/java/qchar/arfind/excel/ExcelTextFinder.java
@@ -1,6 +1,5 @@
 package qchar.arfind.excel;
 
-import com.google.common.base.Verify;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,17 +11,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Basic class for parsing excel files with vendor codes.
  */
 public class ExcelTextFinder implements AutoCloseable {
-    private final static String DELIMITERS = "\\s+|,\\s*|\\.\\s*";
-
     private final String filename;
     private Workbook excelReader;
-    private Sheet currentSheet;
 
     public ExcelTextFinder(String filename) throws IOException {
         this.filename = filename;
@@ -38,32 +33,33 @@ public class ExcelTextFinder implements AutoCloseable {
     }
 
     public List<Row> findMatches(String matchString) {
-        Verify.verify(Objects.nonNull(currentSheet), String.format("Need to set sheet name for %s", filename));
-
         matchString = matchString.toLowerCase();
         List<Row> result = new ArrayList<>();
         String value;
 
-        for (Row currentRow : currentSheet) {
-            for (Cell currentCell : currentRow) {
-                switch (currentCell.getCellTypeEnum()) {
-                    case STRING:
-                        value = currentCell.getStringCellValue().toLowerCase();
+        for (Sheet sheet : excelReader) {
+            for (Row currentRow : sheet) {
+                for (Cell currentCell : currentRow) {
+                    switch (currentCell.getCellTypeEnum()) {
+                        case STRING:
+                            value = currentCell.getStringCellValue().toLowerCase();
 
-                        //for both strings replace difficult letter
-                        value = value.replace('ё', 'е');
-                        matchString = matchString.replace('ё', 'е');
+                            //for both strings replace difficult letter
+                            value = value.replace('ё', 'е');
+                            matchString = matchString.replace('ё', 'е');
 
-                        //needed when users add two whitespaces instead of one
-                        value = value.trim().replaceAll(" +", " ");
-                        if (value.contains(matchString)) {
-                            result.add(currentRow);
-                        }
-                    default:
-                        break;
+                            //needed when users add two whitespaces instead of one
+                            value = value.trim().replaceAll(" +", " ");
+                            if (value.contains(matchString)) {
+                                result.add(currentRow);
+                            }
+                        default:
+                            break;
+                    }
                 }
             }
         }
+
         return result;
     }
 
@@ -83,15 +79,6 @@ public class ExcelTextFinder implements AutoCloseable {
         }
 
         throw new UnknownExcelExtensionException(name);
-    }
-
-    /**
-     * Set sheet for work
-     *
-     * @param name sheet name.
-     */
-    public void workWithSheet(String name) {
-        currentSheet = excelReader.getSheet(name);
     }
 
     @Override
